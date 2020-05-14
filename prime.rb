43af75468efded47d8f9017b89bcc1e094b3560f
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-14 16:22:49 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,29 @@
 # Add  code here!
+
+def prime?(integer)
+  return false if integer < 2 
+  integer_array = (2..integer).to_a
+  integer_array.each do |i|
+    if integer % i == 0
+      return true
+    end
+  end
+end
+
+
+
+# def is_prime?(number)
+#       return false if number <2
+#         (2..Integer.sqrt(number)).each do |divisor|
+#               return false if number % divisor == 0
+#   end
+#     return number
+# end
+
+# def read_array(array)
+#     primes = []
+#   array.each do |num|
+#       primes << num if is_prime?(num)
+#   end
+# return primes
+# end
