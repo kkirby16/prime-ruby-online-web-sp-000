170da495432d19a0ba31fca9cb6c741c223d3d27
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-14 18:19:25 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,11 +1,8 @@
 # Add  code here!
 
 def prime?(number)
-  if !number.integer?
+  if number < 2 
     return false 
-  elsif number < 2 
-    return false 
-  
  if (2..number-1).each { |int| return false if number % int == 0} 
 true
 end
