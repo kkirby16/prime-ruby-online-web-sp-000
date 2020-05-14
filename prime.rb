6cee867c223ffa85b97e51d8850bcaa094d70162
Kevin Kirby <kkirby16@my.whitworth.edu>
2020-05-14 18:21:17 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@
 def prime?(number)
   if number < 2 
     return false 
- elsif (2..number-1).each { |int| number % int == 0} 
+ (2..number-1).each { |int| number % int == 0} 
    return false 
  end
 true
